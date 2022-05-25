906d5d5b5ba2f5dad7e3f147f49bd57b48907c01
Rebecca Tie <rtie@student.unimelb.edu.au>
2022-05-25 21:13:10 +1000

    add infected neighbours check

--- src/Agents/daisy.py
+++ src/Agents/daisy.py
@@ -3,7 +3,7 @@ import random as rd
 from src.Agents.agent import Agent
 
 MAX_AGE = 25
-INIT_INFECT = 0.1
+INIT_INFECT = 0.3
 INFECT = 0.5
 RECOVER = 0.7
 KILL = 0.1
@@ -21,29 +21,27 @@ class Daisy(Agent):
         else: 
             self.infected = 0
 
-    def step(self, temp):
+    def step(self, temp, infected):
         self.age += 1
         # threshold is a parabola with a peak of 1, it drops to 0 at temperatures of 5 degrees and 40 degrees C
         seed_threshold = ((0.1457 * temp) - (0.0032 * (pow(temp,2))) - 0.6443)
-        #Currently assuming always infectious neighour
         r1 = rd.random()
-        if self.infected == 0: 
+        if self.infected == 0 and infected == 1: # susceptible
             if r1 < INFECT:
                 self.infected = 1
-        elif self.infected == 1:
+        elif self.infected == 1: # infected
             if r1 < RECOVER: 
                 self.infected = 2
             if r1 < KILL:
                 self.die()
-        elif self.infected == 2:
+        elif self.infected == 2: # recovered
             if r1 < LOOSE_IMMUNITY:
                 self.infected = 0
-        print(self.infected)
         r2 = rd.random()
         if self.age >= MAX_AGE: return self.die()
         elif r2 < seed_threshold: 
             return self.seed()
-        else: return super().step(temp)
+        else: return super().step(temp, infected)
 
     def seed(self):
         return "seed"
@@ -53,6 +51,9 @@ class Daisy(Agent):
 
     def toString(self):
         return super().toString()
+
+    def getInfectionStatus(self): 
+        return self.infected
 
     @staticmethod
     def getRandAge(self):
